885ad26b84c8a95c0b3d268e863b54232e24bc88
Luke <looklukelemon@gmail.com>
2022-07-10 18:23:55 +0900

    fix:daisy dark

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="dark">
+    <Html className="dark" data-theme="dark">
       <Head />
       <body className="min-h-screen">
         <Main />
