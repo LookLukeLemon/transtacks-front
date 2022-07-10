e7788ffb5542f7b46eeb7193addbd03d1e45a326
Luke <looklukelemon@gmail.com>
2022-07-10 18:45:10 +0900

    fix:faviocon

---
+++ pages/favicon.tsx
@@ -0,0 +1,115 @@
+const Favicon = () => {
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/favicon/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/favicon/apple-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/favicon/apple-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/favicon/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/favicon/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/favicon/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/favicon/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/favicon/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicon/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="application-name" content="Multi sender dope" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Multi sender dope" />
+      <meta name="description" content="Send multiple transactions at once" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta
+        name="msapplication-TileImage"
+        content="/favicon/ms-icon-144x144.png"
+      />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://www.infinitysquare.io" />
+      <meta name="twitter:title" content="Multi sender" />
+      <meta
+        name="twitter:description"
+        content="Send multiple transactions at once"
+      />
+      <meta name="twitter:image" content="/images/ifsq-thumb1.png" />
+      <meta name="twitter:creator" content="Dope" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Multi sender" />
+      <meta
+        property="og:description"
+        content="Send multiple transactions at once"
+      />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:site_name" content="Multi sender" />
+      <meta property="og:image" content="/images/ifsq-thumb1.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:alt" content="Multi sender" />
+      <meta property="og:url" content="https://www.infinitysquare.io" />
+    </>
+  );
+};
+
+export default Favicon;
